297611271ab10032d3e58757c6bd8d50861f609e
jit2xel <9realmsi5@gmail.com>
2026-08-30 04:46:51 +0800

    Use Carto API key for map tiles

--- src/constants/mapTiles.ts
+++ src/constants/mapTiles.ts
@@ -1,17 +1,32 @@
 export const mapTilerKey = process.env.EXPO_PUBLIC_MAPTILER_KEY;
+export const cartoBasemapsKey = process.env.EXPO_PUBLIC_CARTO_BASEMAPS_API_KEY?.trim();
+
+const cartoKeyQuery = cartoBasemapsKey
+  ? `?key=${encodeURIComponent(cartoBasemapsKey)}`
+  : '';
 
 export const darkTileUrl = mapTilerKey
   ? `https://api.maptiler.com/maps/dataviz-dark/256/{z}/{x}/{y}.png?key=${mapTilerKey}`
-  : 'https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
+  : cartoBasemapsKey
+    ? `https://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png${cartoKeyQuery}`
+    : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export const lightTileUrl = mapTilerKey
   ? `https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=${mapTilerKey}`
-  : 'https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
+  : cartoBasemapsKey
+    ? `https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png${cartoKeyQuery}`
+    : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 
 export const mapAttribution = mapTilerKey
   ? 'MapTiler | OpenStreetMap contributors'
-  : 'OpenStreetMap contributors | CARTO';
+  : cartoBasemapsKey
+    ? 'OpenStreetMap contributors | CARTO'
+    : 'OpenStreetMap contributors';
 
-export const staticMapTileUrl = 'https://a.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png';
+export const staticMapTileUrl = cartoBasemapsKey
+  ? `https://basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png${cartoKeyQuery}`
+  : 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 export const fallbackStaticMapTileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
-export const staticMapAttribution = 'OpenStreetMap contributors | CARTO';
+export const staticMapAttribution = cartoBasemapsKey
+  ? 'OpenStreetMap contributors | CARTO'
+  : 'OpenStreetMap contributors';
